2cffa9c5a7aee40365bf073e22ea10e4a724ab04
Sha <shayan@shayan.com>
2019-01-08 12:50:26 +1100

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'progressbar';
+  data = { buttons: [33, 28, -42, -7], bars: [82, 74], limit: 160 };
+  size = 0;
+  isActive = false;
+  barId = 0;
+  barClass = "progress-bar progress-bar-striped progress-bar-animated";
+
+  onClick(num) {
+    this.data.bars[this.barId] += num;
+    if (this.data.bars[this.barId] < 0) {
+      this.data.bars[this.barId] = 0;
+    }
+    if (this.data.bars[this.barId] >= this.data.limit) {
+      this.barClass = "progress-bar progress-bar-striped progress-bar-animated";
+    }
+
+    if (this.data.bars[this.barId] < this.data.limit) {
+      this.barClass = "progress-bar progress-bar-striped progress-bar-animated";
+    }
+  }
+
+  selectBar(id) {
+    this.barId = id;
+  }
 }
